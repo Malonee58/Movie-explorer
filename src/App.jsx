13c78f3d99de15c7ca41c44b0eae8cc82019e1d7
Malonee58 <miraclemalonee58@gmail.com>
2026-02-10 18:07:21 +0100

    Fix homepage and routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,13 @@ function App() {
 			<NavBar />
 			<main className="main-content">
 				<Routes>
+					{/* Default homepage */}
 					<Route path="/" element={<Home />} />
+
+					{/* Redirect /home to / if needed */}
+					<Route path="/home" element={<Navigate to="/" />} />
+
+					{/* Other routes */}
 					<Route path="/Favorites" element={<Favorites />} />
 				</Routes>
 			</main>
